76fce958281e27d3a98a41ab399ec93e3b84a64d
liaowentuan <996002654@qq.comm>
2018-01-03 11:42:25 +0800

    login

---
+++ src/components/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login">
+    <el-row>
+      <el-col :span="8" :offset="8">
+        <el-input placeholder="请输入用户名" v-model="user" clearable></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="8" :offset="8">
+        <el-input placeholder="请输入密码" v-model="password" clearable></el-input>
+      </el-col>
+    </el-row>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        user: '',
+        password: ''
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .login{
+    margin-top:300px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import axios from 'axios'
+import login from '@/components/login'
 
 Vue.prototype.$http = axios
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login
+    },
     {
       path: '/',
       name: 'HelloWorld',
