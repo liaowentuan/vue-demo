bd730c0a4f360a46e24ac62650aeda1d941300d4
liaowentuan <996002654@qq.comm>
2018-01-10 09:58:57 +0800

    box-shadow

--- src/compoment/table/model.vue
+++ src/compoment/table/model.vue
@@ -33,10 +33,38 @@
   right: 0;
   bottom: 0;
   z-index: 500;
-  background: rgba(255, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.7);
 }
 .model-body{
   background:#fff;
   margin: 100px auto 0;
+  box-shadow: 6px 6px 3px #333;
+}
+.model-body header{
+  height:40px;
+  border-bottom:1px solid #ddd;
+  line-height:40px;
+  text-align:center;
+}
+.model-body header button {
+  display:block;
+  float:right;
+  margin-top:8px;
+  margin-right:8px;
+  background:#ddd;
+  border:0;
+  width:24px;
+  height:24px;
+  border-radius:5px;
+  font-size:20px;
+  line-height:20px;
+  color:#fff;
+  cursor:pointer;
+}
+.model-body footer{
+  height:40px;
+  border-top:1px solid #ddd;
+  line-height:40px;
+  text-align:center;
 }
 </style>
