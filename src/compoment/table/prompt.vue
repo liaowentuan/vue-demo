2788b7ca0b981728fc8868e5107c01f71476fba2
liaowentuan <996002654@qq.comm>
2018-01-15 17:24:50 +0800

    prompt

---
+++ src/compoment/table/prompt.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="promptBox">
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'promptBox'
+  }
+</script>
+<style scoped>
+
+</style>
--- src/compoment/table/table.js
+++ src/compoment/table/table.js
@@ -3,6 +3,7 @@ import tableCellComponent from './cell.vue'
 import gridHeaderRowsComponent from './gridHeaderRows.vue'
 import modelComponent from './model.vue'
 import toolTipsComponent from './toolTips.js'
+import promptComponent from './prompt.vue'
 
 const vTable = {
   install: function (Vue) {
@@ -11,6 +12,7 @@ const vTable = {
     Vue.component('gridHeaderRows', gridHeaderRowsComponent)
     Vue.component('vModel', modelComponent)
     Vue.prototype.$toopTips = toolTipsComponent
+    Vue.component('prompt', promptComponent)
   }
 }
 export default vTable
