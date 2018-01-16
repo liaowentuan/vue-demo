bac6fe403f477884ac2aebdd25d2e226c4102b4c
liaowentuan <996002654@qq.comm>
2018-01-16 12:38:22 +0800

    prompt

--- src/compoment/table/prompt.js
+++ src/compoment/table/prompt.js
@@ -4,12 +4,5 @@ const PromptBox = Vue.extend(promptComponent)
 let instance
 
 export default (msg) => {
-  instance = new PromptBox({
-    data: {
-      msg: msg
-    }
-  })
-  instance.vm = instance.$mount()
-  document.body.appendChild(instance.vm.$el)
-  return instance.vm
+
 }
