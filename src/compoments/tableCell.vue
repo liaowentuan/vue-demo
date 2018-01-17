43143169ad78cdfb1dce5326cf5b4e9d32653024
liaowentuan <996002654@qq.comm>
2018-01-17 16:12:27 +0800

    display table

---
+++ src/compoments/tableCell.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="table">
+    <div class="table-tr" v-for="(items, i) in msgData" :key="i">
+      <div class="table-td" v-for="(item, keys) in items" :key="keys">
+        {{keys}}-{{item}}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'tableCell',
+    data () {
+      return {
+        msgData: [
+          {index: 12, msg: 'someError5asdfas1df231a'},
+          {index: 1, msg: 'someError4'},
+          {index: 2, msg: 'someError3'},
+          {index: 3, msg: 'someError2'},
+          {index: 4, msg: 'someError1'}
+        ]
+      }
+    }
+  }
+</script>
+<style>
+  .table{
+    width: 100%;
+    display: table;
+    border-bottom:1px solid #ddd;
+    border-right:1px solid #ddd;
+  }
+  .table-tr{
+    display: table-row;
+  }
+  .table-td{
+    text-align: center;
+    display: table-cell;
+    border-top:1px solid #ddd;
+    border-left:1px solid #ddd;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/compoments/index'
-
+import tableCellComponent from '@/compoments/tableCell'
 import login from '@/compoments/login'
 
 Vue.use(Router)
@@ -17,6 +17,11 @@ export default new Router({
       path: '/index',
       name: 'index',
       component: index
+    },
+    {
+      path: '/tables',
+      name: 'tables',
+      component: tableCellComponent
     }
   ]
 })
