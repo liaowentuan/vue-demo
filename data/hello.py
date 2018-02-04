2712ca8f27c6250be911100964beb2c2e8fb3daa
liaowentuan <996002654@qq.com>
2018-02-04 23:00:10 +0800

    request

--- data/hello.py
+++ data/hello.py
@@ -1,11 +1,13 @@
-from flask import Flask
-import helloDef
+from flask import Flask    # 引入框架
+from flask import request  # 引入request
+import helloDef # 当前目录
 
 app = Flask(__name__)
 
 
 @app.route('/controller/hello', methods=['POST', 'GET'])
 def hello():
+    print(request.data)
     return helloDef.printMe()
 
 if __name__ == '__main__':
