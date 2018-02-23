51724bc5b6e53c36182afc54320f916106ed256e
liaowentuan <996002654@qq.com>
2018-02-23 09:40:15 +0800

    day03

--- data/day02/dict.py
+++ data/day02/dict.py
@@ -8,3 +8,11 @@ print(type(dic.get("5"))) # 安全获取
 del dic['1'] # 删除
 dic.pop("2") # 删除
 print(dic) # 无序打印 那么发給前端也是无序的么？
+
+import random
+print(random.random())
+
+import time
+
+time.sleep(1)
+print('sleep')
